68dfbe14b524c8edeb42307be129987f07b4f1b8
Haoran RAIN <95905185+K0EKJE@users.noreply.github.com>
2023-02-08 16:40:57 -0800

    Update outerproduct.m

--- Utils/outerproduct.m
+++ Utils/outerproduct.m
@@ -2,14 +2,7 @@ function X = outerproduct(varargin)
 % Input: vectors a1, a2, ... an.
 % Output: X, the outer product of a1 o a2 o ... o aN
 %
-% Write a Matlab function that efficiently computes the outer product of any given number of arbitrary
-% vectors.
-% Now use your function to compute the outer product A1=a o b o c (the circle o stands for the outer
-% product) of three random vectors a,b,c with dimensions(=lengths) 4,2,3 respectively. Then compute
-% the other product A2=b o a o c. Are these two tensors identical or different? Can you write Matlab
-% code to transform the second tensor A2 into A1?
-% % 
-
+% can be found @ https://github.com/sderooij/tnkf_lssvm_seizure_detect/blob/56e06bcf3d5d6902e59b0152da9b9656ff2a004c/classification/Tensor_Functions/outerproduct.m
 %% 
 if nargin == 0
     X = [];
